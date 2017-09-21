d011443de6ae180de0ebacae821772bb2e084d1f
beautyfoot <30289523+beautyfoot@users.noreply.github.com>
2017-09-21 19:38:55 +0800

    V0.5 1、修改Play下p_result和l_circle字段，2、增加了第一二轮西甲联赛数据

--- clubApp/admin.py
+++ clubApp/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
 # Register your models here.
+
+
+from clubApp.models import League, Club
+from playApp.models import Play
+
+
+admin.site.register(League)
+admin.site.register(Club)
+admin.site.register(Play)

45f840a2761c7572924fc6901cd1c80bedf4f828
jzh <you@example.com>
2017-09-24 16:29:06 +0800

    建议：play表里添加联赛字段做区分，不然不知道怎么分开两个联赛1,罗列联赛

---
+++ playApp/models.py
@@ -0,0 +1,39 @@
+from django.db import models
+from clubApp.models import Club
+
+
+class Play(models.Model):
+
+    results = (
+        (0, "主队胜"),
+        (1, "客队胜"),
+        (2, "平局"),
+    )
+
+    circles = (
+        (1, "第一轮"),
+        (2, "第二轮"),
+        (3, "第三轮"),
+        (4, "第四轮"),
+        (5, "第五轮"),
+    )
+
+    z_name = models.ForeignKey(Club, verbose_name="主队", related_name="z_name_clubset")
+    k_name = models.ForeignKey(Club, verbose_name="客队", related_name="k_name_clubset")
+    p_time = models.DateTimeField(verbose_name="比赛时间")
+    z_goal = models.SmallIntegerField(verbose_name="主队进球")
+    k_goal = models.SmallIntegerField(verbose_name="客队进球")
+    p_result = models.SmallIntegerField(choices=results, verbose_name="比赛结果")
+    l_circle = models.IntegerField(choices=circles, verbose_name="比赛轮数")
+    z_peilv = models.DecimalField(max_digits=5, decimal_places=3, verbose_name="主赔率")
+    p_peilv = models.DecimalField(max_digits=5, decimal_places=3, verbose_name="平赔率")
+    k_peilv = models.DecimalField(max_digits=5, decimal_places=3, verbose_name="客赔率")
+
+
+
+    def __str__(self):
+        return self.z_name.c_name + ":" + self.k_name.c_name
+
+    class Meta:
+        verbose_name = "比赛"
+        verbose_name_plural = verbose_name
---
+++ playApp/urls.py
@@ -0,0 +1,8 @@
+from django.conf.urls import url
+from . import views
+
+urlpatterns = [
+    url(r'^$', views.LeaguesView.as_view(), name='Leagus'),
+    url(r'^round/(?P<league_num>\d+)/$', views.RoundView.as_view(),name="Round"),
+    url(r'^round/(?P<league_num>\d+)/(?P<round_num>\d+)/$', views.RoundView.as_view(),name="RoundII"),
+]

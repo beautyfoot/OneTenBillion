83253acf4981f1283eb644ce36716203133514cb
zy5622139 <330656987@qq.com>
2017-09-25 17:16:09 +0800

    22222

--- OneTenBillion/settings.py
+++ OneTenBillion/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'clubApp',
     'playApp',
+    'playerApp',
 ]
 
 MIDDLEWARE = [
---
+++ utils/__init__.py
@@ -0,0 +1,7 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+# Date: 2017/9/25
+
+
+
+
